5b5682d7e24bed26456a99f91d1f891c8c58663d
Nicolás Carrillo <nico.carrillodev@icloud.com>
2025-01-27 10:32:45 -0500

    added field price with discount required false

--- src/collections/Orders.ts
+++ src/collections/Orders.ts
@@ -117,6 +117,7 @@ export const pedidos: CollectionConfig = {
           name: 'pricewithDiscount',
           type: 'number',
           label: 'Precio con Descuento',
+          required: false,
         },
         {
           name: 'customTotalPrice',
