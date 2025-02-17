7ee09ca3c4feed460a55f3431c7c9e3845264439
Nicolás Carrillo <nico.carrillodev@icloud.com>
2025-02-17 01:38:13 -0500

    remove access cursos

--- src/collections/Cursos.ts
+++ src/collections/Cursos.ts
@@ -62,7 +62,7 @@ export const Cursos: CollectionConfig = {
   access: {
     read: () => true,
     create: ({ req: { user } }) => Boolean(user && user.role === 'Admin'),
-    update: () => true,
+    update: ({ req: { user } }) => Boolean(user && user.role === 'Admin'),
     delete: ({ req: { user } }) => Boolean(user && user.role === 'Admin'),
   },
 
